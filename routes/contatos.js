85cc632c23e8b0243e7dc19e1614347798533526
Jonathan Jesus <drigues90@gmail.com>
2019-12-30 16:21:54 -0300

    config debug no vscode

--- routes/contatos.js
+++ routes/contatos.js
@@ -1,4 +1,6 @@
 module.exports = function(app){
   var contatos = app.controllers.contatos;
   app.get('/contatos',contatos.index);
+  app.get('/contato/:id',contatos.show);
+  app.post('/contato',contatos.create);
 };
