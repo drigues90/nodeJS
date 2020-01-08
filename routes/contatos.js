3ce4e2eb6c0443ae077428f27ccc3ed07aa1af77
Jonathan Jesus <drigues90@gmail.com>
2020-01-08 13:32:09 -0300

    adcionado filtro de autenticacao dentro de contatos

--- routes/contatos.js
+++ routes/contatos.js
@@ -1,8 +1,9 @@
 module.exports = function(app){
+  var autenticar = require('../middleware/autenticador');
   var contatos = app.controllers.contatos;
-  app.get('/contatos',contatos.index);
-  app.get('/contato/:id',contatos.show);
-  app.post('/contato',contatos.create);
-  app.get('/contato/:id/editar',contatos.edit);
-  app.put('/contato/:id',contatos.update);
+  app.get('/contatos',autenticar, contatos.index);
+  app.get('/contato/:id',autenticar,contatos.show);
+  app.post('/contato',autenticar,contatos.create);
+  app.get('/contato/:id/editar',autenticar,contatos.edit);
+  app.put('/contato/:id',autenticar,contatos.update);
 };
